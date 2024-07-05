d5662d1fcbdcb0e875e9f1254534ed9cc3a9ea66
niko-cxvedadze <nikolozcxvedadze@gmail.com>
2024-07-05 23:54:09 +0400

    #2-profit-calculator

---
+++ code/#2-profit-calculator/app.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var revenue float64
+	var expenses float64
+	var taxRate float64
+
+	fmt.Print("Revenue: ")
+	fmt.Scan(&revenue)
+	
+	fmt.Print("Expenses: ")
+	fmt.Scan(&expenses)
+
+	fmt.Print("Tax Rate: ")
+	fmt.Scan(&taxRate)
+
+
+	earningsBeforeTax := revenue - expenses
+	earningsAfterTax := earningsBeforeTax * (1 - taxRate/100)
+	ration := earningsBeforeTax / earningsAfterTax
+
+	fmt.Println(earningsBeforeTax)
+	fmt.Println(earningsAfterTax)
+	fmt.Println(ration)
+
+	
+}
