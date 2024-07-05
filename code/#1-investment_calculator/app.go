36f9dd78559695f904bd1ef36d8b4362719a4f58
niko-cxvedadze <nikolozcxvedadze@gmail.com>
2024-07-05 23:43:12 +0400

    finish investment calculator

--- code/#1-investment_calculator/app.go
+++ code/#1-investment_calculator/app.go
@@ -6,8 +6,23 @@ import (
 )
 
 func main() {
-	investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5
+	const infaltionRate float64 = 2.5
+	var investmentAmount float64
+	var years float64
+	var expectedReturnRate float64
 
-	futureValue := investmentAmount * math.Pow((1 + expectedReturnRate/100), years)
+	fmt.Print("Investment Amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
+
+	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1 + infaltionRate /100, years);
+
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
