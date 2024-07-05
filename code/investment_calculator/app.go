61700cd7dbd35c8d76687fb6bbf6f0d47caa9096
niko-cxvedadze <nikolozcxvedadze@gmail.com>
2024-07-05 23:09:35 +0400

    variables&data-types

--- code/investment_calculator/app.go
+++ code/investment_calculator/app.go
@@ -1,8 +1,13 @@
 package main
 
-import "fmt"
-
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Print("Investment calculator")
+	investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5
+
+	futureValue := investmentAmount * math.Pow((1 + expectedReturnRate/100), years)
+	fmt.Println(futureValue)
 }
